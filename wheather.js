db8c626cddfad7fb9c70e85bf66bb688a5623d56
MinChan Lee <shigatsu970704@gmail.com>
2020-08-31 11:19:03 +0900

    9.Getting the Weather Part One Geolocation

---
+++ wheather.js
@@ -0,0 +1,39 @@
+const API_KEY = "25a9a94f73898f55f849ed6883521984";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant Access Geo Location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    // get Whether
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
